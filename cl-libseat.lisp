b959b436cde756854654c1d13ebede88d2f7dae8
bmiww <bmiww@bky.one>
2024-05-04 22:36:11 +0200

    Construct the open-seat callback madness

--- cl-libseat.lisp
+++ cl-libseat.lisp
@@ -13,7 +13,6 @@
 
 (use-foreign-library libseat)
 
-
 ;; ┌─┐┌┬┐┬─┐┬ ┬┌─┐┌┬┐┬ ┬┬─┐┌─┐
 ;; └─┐ │ ├┬┘│ ││   │ │ │├┬┘├┤
 ;; └─┘ ┴ ┴└─└─┘└─┘ ┴ └─┘┴└─└─┘
@@ -32,7 +31,7 @@
 ;; ┌─┐┬ ┬┌┐┌┌─┐┌┬┐┬┌─┐┌┐┌┌─┐
 ;; ├┤ │ │││││   │ ││ ││││└─┐
 ;; └  └─┘┘└┘└─┘ ┴ ┴└─┘┘└┘└─┘
-(defcfun ("libseat_open_seat" open-seat) :pointer
+(defcfun ("libseat_open_seat" %open-seat) :pointer
   (listener (:pointer (:struct libseat-seat-listener)))
   (data :pointer))
 
@@ -85,10 +84,41 @@
    (data :pointer))
   (funcall *log-handler-lisp* level format data))
 
+(defvar *enable-seat* nil)
+(defcallback enable-seat-cb :void ((seat :pointer) (data :pointer))
+  (funcall *enable-seat* seat data))
+
+(defvar *disable-seat* nil)
+(defcallback disable-seat-cb :void ((seat :pointer) (data :pointer))
+  (funcall *disable-seat* seat data))
 
 ;; ┬  ┬┌─┐┌─┐
 ;; │  │└─┐├─┘
 ;; ┴─┘┴└─┘┴
+(defun make-libseat-seat-listener (enable-seat-lisp disable-seat-lisp)
+  (setf *enable-seat* enable-seat-lisp *disable-seat* disable-seat-lisp)
+  (let ((listener (foreign-alloc '(:struct libseat-seat-listener))))
+    (with-foreign-slots ((enable-seat disable-seat) listener (:struct libseat-seat-listener))
+      (setf enable-seat (callback enable-seat-cb)
+	    disable-seat (callback disable-seat-cb)))
+    listener))
+
+(defun open-seat (&key enable-seat disable-seat user-data)
+  "Create a new libseat seat context
+Two callbacks are required, via the keys
+:enable-seat and :disable-seat
+
+:enable-seat is called with the seat context and user-data when the seat is enabled
+:disable-seat is called with the seat context and user-data when the seat is disabled
+
+:user-data is a pointer to user data which will be passed to the callbacks,
+the creation of the pointer is left up to the user.
+If :user-data is not provided a null-pointer is used."
+
+  (unless enable-seat (error "enable-seat callback required"))
+  (unless disable-seat (error "disable-seat callback required"))
+  (%open-seat (make-libseat-seat-listener enable-seat disable-seat) (or user-data (null-pointer))))
+
 (defun open-device (seat path)
   (with-foreign-object (fd :int)
     (let ((device-id (open-device seat path fd)))
